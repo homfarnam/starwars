a1ccebb01a359611eaeba5120c6ccf222fe9e4be
homfarnam <homfarnam@gmail.com>
2021-05-30 11:32:54 +0430

    Create app component

---
+++ pages/_app.tsx
@@ -0,0 +1,26 @@
+import "../styles/index.css";
+import "tailwindcss/tailwind.css";
+
+import { ApolloProvider } from "@apollo/react-hooks";
+import withData from "../utils/apollo";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps, apollo }: any) {
+  return (
+    <>
+      <ApolloProvider client={apollo}>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
+  );
+}
+
+// Wraps all components in the tree with the data provider
+export default withData(MyApp);
