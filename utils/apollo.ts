dd10a20d9eb5c3b5522eee300b888c2496b85c26
homfarnam <homfarnam@gmail.com>
2021-05-30 11:32:31 +0430

    Create apollo config file

---
+++ utils/apollo.ts
@@ -0,0 +1,30 @@
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import withApollo from "next-with-apollo";
+import { createHttpLink } from "apollo-link-http";
+import fetch from "isomorphic-unfetch";
+
+// Update the GraphQL endpoint to any instance of GraphQL that you like
+// const GRAPHQL_URL = "https://fe-case-study.vercel.app/api"
+
+const GRAPHQL_URL =
+  process.env.API_URL || "https://fe-case-study.vercel.app/api";
+
+const link = createHttpLink({
+  fetch, // Switches between unfetch & node-fetch for client & server.
+  uri: GRAPHQL_URL + "/graphql",
+});
+
+// Export a HOC from next-with-apollo
+// Docs: https://www.npmjs.com/package/next-with-apollo
+export default withApollo(
+  // You can get headers and ctx (context) from the callback params
+  // e.g. ({ headers, ctx, initialState })
+  ({ initialState }) =>
+    new ApolloClient({
+      link: link,
+      cache: new InMemoryCache()
+        //  rehydrate the cache using the initial data passed from the server:
+        .restore(initialState || {}),
+    })
+);
