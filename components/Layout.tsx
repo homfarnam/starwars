3b8a635582d90fe4ee00b88472186103665fc040
homfarnam <homfarnam@gmail.com>
2021-05-30 11:35:29 +0430

    Add layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,33 +1,30 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Head from "next/head";
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
+    <header className="w-full flex">
+      <nav className="flex w-6/12 justify-around m-auto py-2">
         <Link href="/">
           <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
+        </Link>
+        <Link href="/login">
+          <a>Login</a>
+        </Link>
+        <Link href="/signup">
+          <a>Sign up</a>
+        </Link>
       </nav>
     </header>
     {children}
@@ -36,6 +33,6 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <span>I'm here to stay (Footer)</span>
     </footer>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
