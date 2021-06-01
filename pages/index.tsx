69c1150c11b2711a3afe6fa2ddc28b6617308cf4
homfarnam <homfarnam@gmail.com>
2021-06-01 17:08:13 +0430

    Delete unused

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,6 @@
-import { useQuery } from "@apollo/react-hooks"
 import Layout from "@components/Layout"
-import { PEOPLE, USER } from "graphql/Queries"
 
 const IndexPage = () => {
-  const { data, loading, error } = useQuery(PEOPLE)
-
-  console.log("data: ", { data })
-
-  if (error) console.log({ error })
-
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <div className="flex w-full justify-center items-start h-[100px] text-3xl">
