ca3600d935d51a5cb1adbcbfcc2e220d943c1079
homfarnam <homfarnam@gmail.com>
2021-06-01 17:07:28 +0430

    Change header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,42 +2,57 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { AUTH_TOKEN } from "types/types.d"
+import { useQuery } from "@apollo/react-hooks"
+import { USER } from "graphql/Queries"
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
   const [AuthToken, setAuthToken] = useState<any>()
   const router = useRouter()
+  const { data, error } = useQuery(USER)
+
+  console.log("user: ", data)
 
   useEffect(() => {
     setAuthToken(localStorage.getItem(AUTH_TOKEN))
   }, [])
+
   return (
     <header className="w-full flex bg-[#3fcdad]">
-      <nav className="flex w-6/12 justify-around m-auto py-2">
-        <Link href="/">
-          <a>Home</a>
-        </Link>
+      <nav className="flex w-full justify-center m-auto py-2">
+        <div className="w-4/12"></div>
+        <div className="flex justify-center space-x-5 text-lg w-4/12">
+          <Link href="/home">
+            <a>Home</a>
+          </Link>
 
-        {!AuthToken && (
+          {!AuthToken && (
+            <div className="flex">
+              <Link href="/signup">Sign up</Link>
+            </div>
+          )}
+
+          {AuthToken ? (
+            <div
+              className="cursor-pointer"
+              onClick={() => {
+                localStorage.removeItem(AUTH_TOKEN)
+                router.push(`/`)
+                router.reload()
+              }}
+            >
+              logout
+            </div>
+          ) : (
+            <Link href="/login">login</Link>
+          )}
+        </div>
+        <div className="flex  w-4/12">
           <div className="flex">
-            <Link href="/signup">Sign up</Link>
+            <span>User: </span> <p> {data?.me?.email}</p>
           </div>
-        )}
-
-        {AuthToken ? (
-          <div
-            className="cursor-pointer"
-            onClick={() => {
-              localStorage.removeItem(AUTH_TOKEN)
-              router.push(`/home`)
-            }}
-          >
-            logout
-          </div>
-        ) : (
-          <Link href="/login">login</Link>
-        )}
+        </div>
       </nav>
     </header>
   )
