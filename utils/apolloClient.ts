6791084c1da6e05ef63d1645887924bec3e2ca70
homfarnam <homfarnam@gmail.com>
2021-06-05 11:50:52 +0430

    Create new apollo config file

---
+++ utils/apolloClient.ts
@@ -0,0 +1,83 @@
+import { useMemo } from "react"
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
+import { concatPagination } from "@apollo/client/utilities"
+import merge from "deepmerge"
+import isEqual from "lodash/isEqual"
+import { AUTH_TOKEN } from "types/types.d"
+import Cookies from "js-cookie"
+
+export const APOLLO_STATE_PROP_NAME = "__APOLLO_STATE__"
+
+let apolloClient: any = null
+
+const GRAPHQL_URL =
+  process.env.API_URL || "https://fe-case-study.vercel.app/api"
+
+const newToken = Cookies.get(AUTH_TOKEN)
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === "undefined",
+    link: new HttpLink({
+      uri: GRAPHQL_URL + "/graphql", // Server URL (must be absolute)
+      credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
+      headers: {
+        authorization: newToken ? `${newToken}` : "",
+      },
+    }),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            allPosts: concatPagination(),
+          },
+        },
+      },
+    }),
+  })
+}
+
+export function initializeApollo(initialState: any = null) {
+  const _apolloClient = apolloClient ?? createApolloClient()
+
+  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
+  // gets hydrated here
+  if (initialState) {
+    // Get existing cache, loaded during client side data fetching
+    const existingCache = _apolloClient.extract()
+
+    // Merge the existing cache into data passed from getStaticProps/getServerSideProps
+    const data = merge(initialState, existingCache, {
+      // combine arrays using object equality (like in sets)
+      arrayMerge: (destinationArray, sourceArray) => [
+        ...sourceArray,
+        ...destinationArray.filter((d) =>
+          sourceArray.every((s) => !isEqual(d, s))
+        ),
+      ],
+    })
+
+    // Restore the cache with the merged data
+    _apolloClient.cache.restore(data)
+  }
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === "undefined") return _apolloClient
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient
+
+  return _apolloClient
+}
+
+export function addApolloState(client: any, pageProps: any) {
+  if (pageProps?.props) {
+    pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract()
+  }
+
+  return pageProps
+}
+
+export function useApollo(pageProps: any) {
+  const state = pageProps[APOLLO_STATE_PROP_NAME]
+  const store = useMemo(() => initializeApollo(state), [state])
+  return store
+}
